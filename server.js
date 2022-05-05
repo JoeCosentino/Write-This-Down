2929c8f6fec88f15aabaf236e8fdd24da899c040
Joey Cos <joeycosentino24@gmail.com>
2022-05-05 10:16:07 -0400

    added parameter get route, comitting for askbcs

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 const { data } = require('./Develop/db/db.json');
 
+function findById(id, dataArray) {
+    console.log(dataArray);
+    const result = dataArray.filter(note => note.id === id);
+    return result;
+}
+
 app.get('/api/data', (req, res) => {
     let results = data;
-    console.log(req.query);
+    // console.log(req.query);
     res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.get('api/data/:id', (req, res) => {
+    const result = findById(req.param.id, data);
+    console.log(data);
+    console.log(result);
+    res.json(result);
+})
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
